968d53d295a2e1daccc281d6106b1af8f037410f
John Zaia <john.zaia@outlook.com>
2025-03-09 16:29:43 -0400

    added a add_records function which allows the user to add records to the db

--- controller.js
+++ controller.js
@@ -6,4 +6,19 @@ router.get("/", async (req, res) => {
     res.render("homepage");
 });
 
+router.post("/add", async (req, res) =>{
+
+    const record = {
+        album_name: req.body.album_name,
+        artist: req.body.artist,
+        genre: req.body.genre,
+        year: req.body.year,
+        rating: req.body.rating,
+        condition: req.body.condition,
+        price: req.body.price
+        };
+        await model.add_record(record);
+        res.redirect("/");
+})
+
 module.exports = router;
--- model.js
+++ model.js
@@ -9,6 +9,13 @@ async function startup()
     })
 }
 
+async function add_record(record){
+    await db.run(`INSERT INTO Records (album_name, artist, genre, year, rating, condition, price)
+                   VALUES (?, ?, ?, ?, ?, ?, ?)`,
+                   [record.album_name, record.artist, record.genre, record.year, record.rating, record.condition, record.price]);
+}1
+
 module.exports = {
-    startup
+    startup,
+    add_record
 }
